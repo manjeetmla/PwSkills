c658373ec37bacfd4dc051907040df2159ee143b
MANJEET KUMAR <manjeetkumarskills@gmail.com>
2023-01-20 20:45:00 +0530

    Add files via upload

---
+++ areaOfRect.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// area of rectangle
+int main(){
+    int length=10;
+    int breadth=20;
+    cout <<"Lenght "<< length << endl;
+    cout<< "Breadth" << breadth << endl;
+    int area = length*breadth;
+    cout << "Area is " << area << endl;
+    return 0;
+}
