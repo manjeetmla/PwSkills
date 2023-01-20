fde36518cc459597242b78f35ac2a2d547410734
MANJEET KUMAR <manjeetkumarskills@gmail.com>
2023-01-20 20:04:55 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+/* take 2 integer values in two variables
+x and y and print their product */
+
+int main(){
+
+    int x,y,c;
+
+    cout<<"x:";
+    cin>>x;
+    cout<<"y:";
+    cin>>y;
+    
+    c=x*y;
+    cout<<"x*y="<<c<<endl;
+    return 0;
+    
+}
